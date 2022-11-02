20dbe3622d03011ba89a1b4884318c7381b253a9
gj2lee <117241224+gj2lee@users.noreply.github.com>
2022-11-02 11:14:30 +0900

    Update firstCommit.c

--- firstCommit.c
+++ firstCommit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "secondfile.h"
 void main(void)
 {
     int i, j;
@@ -6,4 +7,6 @@ void main(void)
     i = 1;
     j=i+2;
     printf("i=%d, j=%d \n", i, j);
+    i= calculate (i, j);
+    printf("calculated result is %d \n", i);
 }
