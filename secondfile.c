161b65409b29ad22ab67789b702574208f9d47be
gj2lee <117241224+gj2lee@users.noreply.github.com>
2022-11-02 11:11:20 +0900

    Create secondfile.c

---
+++ secondfile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+int calculate (int i, int j)
+{
+    int ret;
+    int i=2;
+    int j=3;
+  
+  
+    return (i+j-1); 
+  
+}
